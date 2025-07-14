8fa34e1e1a5e9822b79c0f49e3746bce146960e4
skiper24 <manumascardi06@gmail.com>
2025-07-14 12:14:32 -0300

    Ajuste quienes somos

--- js/carrusel.js
+++ js/carrusel.js
@@ -1,7 +1,5 @@
 const slide = document.querySelector(".carrusel-slide");
 const images = document.querySelectorAll(".carrusel-slide img");
-const prevBtn = document.querySelector(".prev");
-const nextBtn = document.querySelector(".next");
 
 let currentIndex = 0;
 const totalSlides = images.length;
@@ -10,21 +8,9 @@ function updateSlide() {
   slide.style.transform = `translateX(-${currentIndex * 100}%)`;
 }
 
-nextBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  currentIndex = (currentIndex + 1) % totalSlides;
-  updateSlide();
-});
-
-prevBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
-  updateSlide();
-});
-
-
 // Cambio automático cada 5 segundos
 setInterval(() => {
   currentIndex = (currentIndex + 1) % totalSlides;
   updateSlide();
 }, 5000);
+
